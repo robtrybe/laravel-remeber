9ebe949adccf2a389df5fdcb17fe51200262d10b
Robson Narcizo <rob.narcizo.27@gmail.com>
2022-09-17 11:01:33 -0300

    Definindo rota usando Classe

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     echo 'Seja Bem Vindo!';
 });
+
+#Definindo rota usando Classes
+Route::get('/contact',[ContactController::class,'contact']);
